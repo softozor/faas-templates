47e5b735370064b216cece939b7b07a9881f7892
Laurent Michel <laurent.michel@softozor.ch>
2021-02-11 16:08:15 +0100

    * add x-hasura-admin-secret to the session variables * update package number

--- lib/HasuraHandling/Data/HasuraSessionVariables.cs
+++ lib/HasuraHandling/Data/HasuraSessionVariables.cs
@@ -10,5 +10,8 @@ namespace HasuraHandling.Data
 
     [JsonProperty("x-hasura-role")]
     public string Role { get; set; }
+
+    [JsonProperty("x-hasura-admin-secret")]
+    public string AdminSecret { get; set; }
   }
 }
